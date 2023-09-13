7a59155697a011e63975f0f1cc26c749e7a7e44a
Fatma Hussein <fartunhussein346@gmail.com>
2023-09-13 13:48:37 +0300

    Add styling to readme element

--- src/components/detailsPage.js
+++ src/components/detailsPage.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import ReactReadMoreReadLess from 'react-read-more-read-less';
 import '../styles/dets.css';
 import BackArrow from './BackArrow';
 
@@ -19,7 +20,14 @@ function DetailsPage() {
         <div className="img">
           <img alt="prod" src={prod.image} />
         </div>
-        <p className="desc">{prod.description}</p>
+        <ReactReadMoreReadLess
+          charLimit={40}
+          readMoreText="Read more ▼"
+          readLessText="Read less ▲"
+        >
+          {prod.description}
+        </ReactReadMoreReadLess>
+
         <p className="prices">
           $
           {prod.price}
